6b0a511a1ad1f50c1372b6aeff8506ddb145f60a
Chad Williams <chadgit13@gmail.com>
2016-11-29 03:55:56 +0000

    Authorization, pagination

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,21 @@ class UsersController < ApplicationController
     # debugger
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      # Handle a successful update.
+      flash[:success] = "Profile updated"
+      redirect_to@user
+    else
+      render 'edit'
+    end
+  end
+
   def new
     @user = User.new
   end
